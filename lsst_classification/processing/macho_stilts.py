495280bb7cc0706ff0d49530db978436cf3c616d
Ryan McCall <mccall.ryan@gmail.com>
2017-12-13 15:11:50 -0800

    macho acq via stilts working with logging

--- lsst_classification/processing/macho_stilts.py
+++ lsst_classification/processing/macho_stilts.py
@@ -5,6 +5,13 @@ import subprocess
 from subprocess import CalledProcessError
 import time
 
+from prettytable import PrettyTable
+
+from lsst_classification.utils.basic_logging import getBasicLogger
+
+
+logger = getBasicLogger(__name__, __file__)
+
 
 def genList(start, end):
     return list(range(start, end + 1))
@@ -17,7 +24,7 @@ def main():
     commandBase = [jreBinary, "-jar", jarPath, "tapquery"]
     commandBase += ["tapurl=http://machotap.asvo.nci.org.au/ncitap/tap"]
 
-    queryLimit = 500000
+    returnedLimit = 500000
     limit = int(10e7)
     # testQuery = "SELECT TOP 10 * FROM public.star_view"
     joinQuery = ("SELECT TOP %s b.classification, a.fieldid, a.tileid, a.seqn, " 
@@ -33,14 +40,14 @@ def main():
     # fields = [1, 2]
 
     # fields based on data shown at http://macho.nci.org.au/macho_photometry/
-    fields = (genList(1, 180) + genList(206, 208) + genList(211, 213) +
+    fields = (genList(25, 180) + genList(206, 208) + genList(211, 213) +
               genList(301, 311) + genList(401, 403))
     categoryStart, categoryEnd = 1, 11
     classCounts = defaultdict(int)
     allStart = time.time()
     for field in fields:
         for cat in range(categoryStart, categoryEnd + 1):
-            print("\nField: %s Class: %s" % (field, cat))
+            logger.info("Field: %s Class: %s", field, cat)
             outPath = os.path.join(outDir, "c%s_f%s.csv" % (cat, field))
             fullQuery = joinQuery % (limit, field, cat)
             cmd = commandBase + ["adql=" + fullQuery, "out=" + outPath,
@@ -48,13 +55,13 @@ def main():
             apiStart = time.time()
             try:
                 output = subprocess.check_output(cmd)
-            except CalledProcessError as e:
-                print(e)
+            except CalledProcessError:
+                logger.exception("JAR call failed")
                 return
 
-            print("call took: %.01fs" % (time.time() - apiStart))
+            logger.debug("call took: %.01fs", time.time() - apiStart)
             if output:
-                print("subprocess output: %s" % output.decode("utf-8"))
+                logger.debug("subprocess output: %s", output.decode("utf-8"))
 
             # if outfile is empty, print a warning and delete it
             with open(outPath, "r") as outFile:
@@ -62,19 +69,36 @@ def main():
 
             classCounts[cat] += outLineCount
             if outLineCount == 1:
-                print("result: %s too small" % field)
+                logger.info("Skipping empty result")
                 os.remove(outPath)
 
-            if outLineCount >= queryLimit:
-                print("WARNING: Reach limit! Data likely missed: %s" %
-                      outLineCount)
+            if outLineCount >= returnedLimit:
+                logger.warning("Reached TAP limit! Data likely missed: %s",
+                               outLineCount)
 
-    print("cat\tcounts\tpercentage")
+    t = PrettyTable(["Category", "Counts", "Percentage"])
     totalCounts = sum(classCounts.values())
     for cat, counts in sorted(classCounts.items()):
-        print("%s\t%s\t%.02f" % (cat, counts, 100 * counts / totalCounts))
+        t.add_row([cat, counts, round(100.0 * counts / totalCounts, 2)])
 
-    print("Entire harvest took: %.01fm" % (time.time() - allStart) / 60)
+    # +----------+---------+------------+
+    # | Category | Counts | Percentage |
+    # +----------+---------+------------+
+    # | 1 | 2668376 | 32.29 |
+    # | 2 | 612715 | 7.41 |
+    # | 3 | 111089 | 1.34 |
+    # | 4 | 619357 | 7.49 |
+    # | 5 | 318188 | 3.85 |
+    # | 6 | 55188 | 0.67 |
+    # | 7 | 152359 | 1.84 |
+    # | 8 | 352080 | 4.26 |
+    # | 9 | 187325 | 2.27 |
+    # | 10 | 3048492 | 36.89 |
+    # | 11 | 138465 | 1.68 |
+    # +----------+---------+------------+
+
+    logger.info(t)
+    logger.info("Entire harvest took: %.01fm", (time.time() - allStart) / 60)
 
 
 if __name__ == "__main__":
---
+++ lsst_classification/utils/basic_logging.py
@@ -0,0 +1,32 @@
+import logging
+import os
+import sys
+
+
+LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
+
+DATE_FORMAT = "%H:%M:%S"
+
+
+def getBasicLogger(name, fileName,
+                   format=LOG_FORMAT,
+                   dateFmt=DATE_FORMAT,
+                   level=logging.INFO):
+    outFile = fileName.split(os.path.sep)[-1]
+    outFile = outFile.replace("py", "log")
+    logPath = os.path.join(os.environ.get("LSST"), "logs", outFile)
+    logging.basicConfig(filename=logPath,
+                        filemode="a",
+                        format=format,
+                        datefmt=dateFmt,
+                        level=level)
+
+    logger = logging.getLogger(name)
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    handler.setFormatter(logging.Formatter(format))
+    logger.addHandler(handler)
+
+    return logger
