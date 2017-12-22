58757de00c60eac9ba2907f6b59084d76c2924d0
Ryan McCall <mccall.ryan@gmail.com>
2017-12-21 18:40:16 -0800

    multiprocessing script to determine how much ogle3 data can be fetched

--- light_curve_ml/processing/peek_ogle.py
+++ light_curve_ml/processing/peek_ogle.py
@@ -1,31 +1,64 @@
+import argparse
+import multiprocessing
+from multiprocessing import Pool
+import time
+
 from feets.datasets import macho
 from feets.datasets.ogle3 import load_OGLE3_catalog, fetch_OGLE3
-from feets.extractors.core import DATA_TIME, DATA_MAGNITUDE, DATA_ERROR
+from feets.extractors.core import DATA_TIME
+import requests
 
 
 def peekMacho():
-    """Only provides small number of macho's"""
+    """Only provides small number of macho ids"""
     print(macho.available_MACHO_lc())
 
 
+def fetchWork(vid):
+    try:
+        return fetch_OGLE3(vid)
+    except requests.RequestException:
+        return None
+
+
+def _getArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--limit", type=int, default=int(1e20),
+                        help="Limit script to running first n things")
+    return parser.parse_args()
+
+
 def peekOgle():
-    if 1:
-        df = load_OGLE3_catalog()
-        # print(list(df))
-        validIds = [id for id in df["ID"] if id != "-99.99"]
-        print("Valid OGLE3 ids: %s" % len(validIds))
-        count = 0
-        for vid in validIds:
-            bunch = fetch_OGLE3(vid)
-            if bunch and bunch.bands:
-                for subBunch in bunch.bands.values():
-                    if len(subBunch[DATA_TIME]):
-                        count += 1
+    sAll = time.time()
+    args = _getArgs()
+    df = load_OGLE3_catalog()
+    ids = [id for id in df["ID"] if id != "-99.99"]
 
-        print("Valid rate %s / %s" % (count, len(validIds)))
+    SUFFICIENT_LENGTH = 50
+    validData = 0
+    insufficient = 0
+    bunchNone = 0
+    pool = Pool(processes=multiprocessing.cpu_count())
+    s = time.time()
+    bunches = pool.map(fetchWork, ids[:args.limit])
+    for bunch in bunches:
+        if bunch and bunch.bands:
+            for subBunch in bunch.bands.values():
+                if len(subBunch[DATA_TIME]) > SUFFICIENT_LENGTH:
+                    validData += 1
+                else:
+                    insufficient += 1
 
-    if 0:
-        print(fetch_OGLE3("OGLE-BLG-LPV-228732"))
+        elif bunch is None:
+            bunchNone += 1
+
+    print("Computation elapsed: %.2fs" % (time.time() - s))
+    print("All elapsed: %.2fs" % (time.time() - sAll))
+    print("Total OGLE3 ids: %s" % len(ids))
+    validRate = "{:.3%}".format(validData / len(ids))
+    insuffRate = "{:.3%}".format(insufficient / len(ids))
+    print("Valid: %s (%s) insufficient length: %s (%s) bunchNone: %s" % (
+        validData, validRate, insufficient, insuffRate, bunchNone))
 
 
 if __name__ == "__main__":
---
+++ light_curve_ml/utils/multiprocessing_example.py
@@ -0,0 +1,61 @@
+import multiprocessing
+from multiprocessing import Pool
+import time
+
+
+def workFunc(taskQueue, resultQueue, *args):
+    while True:
+        nextTask = taskQueue.get()
+        if nextTask is None:
+            break
+
+        resultQueue.put(nextTask["v"] * args[0])
+
+
+def multiprocessingQueueTest(problems):
+    # Establish communication queues
+    taskQueue = multiprocessing.JoinableQueue()
+    resultQueue = multiprocessing.Queue()
+
+    for p in range(problems):
+        taskQueue.put({"v": p})
+
+    numCPU = multiprocessing.cpu_count()
+    for _ in range(numCPU):
+        taskQueue.put(None)
+
+    a = 2
+    jobs = []
+    for i in range(numCPU):
+        p = multiprocessing.Process(target=workFunc,
+                                    args=(taskQueue, resultQueue, a))
+        jobs.append(p)
+        p.daemon = True
+        p.start()
+
+    # taskQueue.join()
+    while not taskQueue.empty():
+        print("waiting...")
+        time.sleep(1)
+
+    sum = 0
+    while not resultQueue.empty():
+        sum += resultQueue.get()
+
+    print("sum %s" % sum)
+
+
+def f(a):
+    return 2 * a
+
+
+def testTheWaters(size):
+    pool = Pool(processes=6) # run no more than 6 at a time
+
+    s = time.time()
+    result = pool.map(f, range(size)) # pass full list (12 items)
+    total = sum(result)
+    print("%s in %.2fs" % (total, time.time() - s))
+
+if __name__ == "__main__":
+    testTheWaters(int(1e5))
