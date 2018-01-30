d9053bdcf4bd510e68dae198961272d1def7abab
Ryan McCall <mccall.ryan@gmail.com>
2018-01-29 17:35:54 -0800

    minor fixes to ogle download

--- lcml/processing/ogle/down_ogle.py
+++ lcml/processing/ogle/down_ogle.py
@@ -19,6 +19,9 @@ logger = getBasicLogger(__name__, "peek_ogle.log")
 SUFFICIENT_LENGTH = 60
 
 
+_REPORT_FREQ = 100
+
+
 def _getArgs():
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--start", type=int, default=0,
@@ -32,46 +35,57 @@ def _getArgs():
     return parser.parse_args()
 
 
+def reportDownloaded():
+    downDir = os.path.join(os.path.expandvars("$HOME"), "feets_data/ogle3")
+    _dir = [f for f in os.listdir(downDir)
+            if f.endswith(".tar")]
+    print("found %d tarfiles" % len(_dir))
+
+
 @retry(timeoutSec=120, initialRetryDelaySec=1, maxRetryDelaySec=100,
-       retryExceptions=(requests.RequestException,), logger=logger)
+       retryExceptions=(requests.RequestException,))
 def fetchOgle3(vid):
+    result = None
     try:
-        return fetch_OGLE3(vid)
+        result = fetch_OGLE3(vid)
     except tarfile.ReadError:
         print("error reading tarfile: %s" % vid)
-        raise
+    except ValueError as e:
+        print("Error occurred fetching file: %s" % e)
+        print("Skipping vid: %s" % vid)
+
+    return result
 
 
-_REPORT_FREQ = 100
-
-
-def downOgle():
+def main():
+    reportDownloaded()
     sAll = time.time()
     args = _getArgs()
     df = load_OGLE3_catalog()
     ids = [i for i in df["ID"] if i != "-99.99"]
     print("Found %d OGLE3 ids" % len(ids))
+    requestedIds = ids[args.start: args.end]
 
     validLc = 0
     tooShortLc = 0
 
     s = time.time()
-    # feets library data structures fail in a multiprocess setting
     # pool = Pool(processes=multiprocessing.cpu_count())
-    # datas = pool.map(ogleOrNone, ids[:args.limit])
+    # datas = pool.map(fetchOgle3, requestedIds)
 
     c = args.start
-    for vid in ids[args.start: args.end]:
+    for vid in requestedIds:
         if c % _REPORT_FREQ == 0:
             logger.info("%s / %s", c, args.end)
 
         c += 1
         dataObj = fetchOgle3(vid)
-        for bandChar, band in dataObj.data.items():
-            if len(band.time) > args.sufficientLength:
-                validLc += 1
-            else:
-                tooShortLc += 1
+        if dataObj:
+            for bandChar, band in dataObj.data.items():
+                if len(band.time) > args.sufficientLength:
+                    validLc += 1
+                else:
+                    tooShortLc += 1
 
     logger.info("\nScript total elapsed: %.2fs",time.time() - sAll)
     logger.info("Process data elapsed: %.2fs\n", time.time() - s)
@@ -84,16 +98,8 @@ def downOgle():
     logger.info("Valid data: %s (%s)\nInsufficient length: %s (%s)", validLc,
         validRate, tooShortLc, insuffRate)
 
-
-def reportDownloaded():
-    downDir = os.path.join(os.path.expandvars("$HOME"), "feets_data/ogle3")
-    _dir = [f for f in os.listdir(downDir)
-            if f.endswith(".tar")]
-    print("found %d tarfiles" % len(_dir))
+    reportDownloaded()
 
 
 if __name__ == "__main__":
-    reportDownloaded()
-    downOgle()
-    reportDownloaded()
-    # print(fetch_OGLE3("OGLE-LMC-ACEP-001").data.I.time)
+    main()
