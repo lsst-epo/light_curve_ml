1e5ebd048c5b55577a41c93c1362e44cd0d542db
Ryan McCall <mccall.ryan@gmail.com>
2018-03-13 17:50:42 -0700

    unsupervised learning research

---
+++ lcml/poc/unsupervised/performance_evaluation.py
@@ -0,0 +1,33 @@
+"""
+A. Given the knowledge of the ground truth class assignments
+
+- Adjusted Rand index
+from sklearn import metrics
+metrics.adjusted_rand_score(labels_true, labels_pred)
+
+- Mutual Information based scores
+from sklearn import metrics
+metrics.adjusted_mutual_info_score(labels_true, labels_pred)
+metrics.normalized_mutual_info_score(labels_true, labels_pred)
+
+homogeneity: each cluster contains only members of a single class.
+completeness: all members of a given class are assigned to the same cluster.
+V = f(homogeneity, completeness)
+V == normalized_mutual_info !!
+sklearn.metrics.homogeneity_completeness_v_measure
+
+NB. prefer adjusted rand score and adjusted mutual information for samples
+less than 1,000 or clusters > 10
+
+sklearn.metrics.fowlkes_mallows_score
+
+B. No labled data
+
+- Silhouette Coefficient
+kmeans_model = KMeans(n_clusters=3, random_state=1).fit(X
+labels = kmeans_model.labels_
+metrics.silhouette_score(X, labels, metric='euclidean')
+
+Calinski-Harabaz Index
+metrics.calinski_harabaz_score(X, labels)
+"""
--- lcml/poc/unsupervised/sklearn_kmeans.py
+++ lcml/poc/unsupervised/sklearn_kmeans.py
@@ -7,10 +7,12 @@ def m():
     xIris = iris.data
     yIris = iris.target
 
+    # xIris has dimensionality: [n_samples, n_features]
     k_means.fit(xIris)
     clustering = k_means.labels_[::10]
     print(clustering)
     print(yIris[::10])
+
 
 if __name__=="__main__":
     m()
