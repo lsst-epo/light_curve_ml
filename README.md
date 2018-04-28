c6c45c52ae8a114737d2fb62ce8ed6cdef87e653
Ryan McCall <mccall.ryan@gmail.com>
2018-04-27 19:52:38 -0700

    job file sections description and instructions how to install on ubuntu

--- README.md
+++ README.md
@@ -20,6 +20,18 @@ configuration of the pipeline and detailed declaration of experiment parameters.
 The specified job file supercedes the default job file on a per field basis recursively. 
 So any, or none, of the default fields may be overridden. The default settings are located
  at `conf/common/pipeline.json`. 
+ 
+### Sections
+Job files have the following structure:
+- `globalParams` - parameters used across multiple pipeline stages
+- `database` - all db config and table names
+- `loadData` - config pertaining to the loading and preprocessing of raw data
+- `extractFeatures` - feature extraction config
+- `modelSearch` - specifies which ML models to test out
+-- `function` - search function name
+-- `model` - ML model spec including non-searched parameters
+-- `params` - parameters controlling the model search
+- `serialization` - controls the persiting of ML models and their metadata to disk
 
 # Other Scripts
 - `lcml.data.acquisistion` - Scripts used to acquire and/or process various datasets including MACHO, OGLE3, Catalina, and Gaia
--- conf/dev/deploy.sh
+++ conf/dev/deploy.sh
@@ -1,39 +1,43 @@
+# Instructions for how to install `lcml` on Ubuntu 16
+
+# OS libraries
 sudo apt update
 sudo apt install python3-minimal python3-pip sqlite3 p7zip-full python3-tk
-pip install --upgrade pip
 
->>> import matplotlib
->>> matplotlib.matplotlib_fname()
-vim [fname]
-# switch backend to: Agg
+# mount elastic block store
+sudo mkfs -t ext4 /dev/xvdb
+sudo mkdir /data
+sudo mount /dev/xvdb /data
+sudo chown ubuntu /data
 
+# git convenience
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
 
-cd $HOME
-git clone https://github.com/carpyncho/feets.git
-git clone https://github.com/lsst-epo/light_curve_ml.git
-
-sudo mkfs -t ext4 /dev/xvdb      (‘xvdb’ maps to ‘sdb’ in the “Device” value in the screenshot above; change if needed)
-sudo mkdir /data
+# shell environment
+vim ~/.bashrc
+export LCML=/home/ubuntu/light_curve_ml
+export PYTHONPATH=/home/ubuntu/light_curve_ml:$PYTHONPATH
+alias python=/usr/bin/python3
+alias pip=/usr/bin/pip3
 sudo mount /dev/xvdb /data
-sudo chown ubuntu /data
+
+# install lcml
+cd $HOME
+git clone https://github.com/lsst-epo/light_curve_ml.git
+cd $LCML
+mkdir logs
+pip install -e . --user
+
+# ensure plotting will work
+python
+>>> import matplotlib
+>>> matplotlib.matplotlib_fname()
+vim [fname]
+# switch backend property to: Agg
 
 # copy dirs recursively into EBS mount
 scp -rp -i ~/.ssh/rjm_lsst_2018.pem ogle3/ ubuntu@ec2-34-215-180-36.us-west-2.compute.amazonaws.com:/data
 
-# in ~/.bashrc
-vim ~/.bashrc
-export LCML=/home/ubuntu/light_curve_ml
-export PYTHONPATH=/home/ubuntu/light_curve_ml:$PYTHONPATH
-
-alias python=/usr/bin/python3
-
-cd $LCML
-mkdir logs
-pip install -r requirements.txt
-
-cd $HOME/feets
-pip install -e .
